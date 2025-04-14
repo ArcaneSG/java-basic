e083f7f26b568f410322c75791df78923f05ebd3
arcanesg <arcanesg@gmail.com>
2025-04-14 23:11:58 +0900

    클래스가 필요한 이유

---
+++ src/class1/ClassStart1.java
@@ -0,0 +1,16 @@
+package class1;
+
+public class ClassStart1 {
+    public static void main(String[] args) {
+        String student1Name = "John 1";
+        int student1Age = 18;
+        int student1Grade = 90;
+
+        String student2Name = "Jane 2";
+        int student2Age = 20;
+        int student2Grade = 80;
+
+        System.out.println("이름 : " + student1Name + " 나이 : " + student1Age + " 성적 : " + student1Grade);
+        System.out.println("이름 : " + student2Name + " 나이 : " + student2Age + " 성적 : " + student2Grade);
+    }
+}
